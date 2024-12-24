b0326254cc08cb1a808a104eb96f7ef0d698e05f
roxxe3 <hamzafarissi@gmail.com>
2024-12-24 18:48:33 +0100

    Add detailed documentation for the convert function in index.js

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const massP = document.getElementById("mass")
 const convertBtn = document.getElementById("btn")
 
 
+/**
+ * Converts the input number to various units of length, volume, and mass,
+ * and updates the corresponding HTML elements with the converted values.
+ * 
+ * The function performs the following conversions:
+ * - Length: meters to feet and feet to meters
+ * - Volume: liters to gallons and gallons to liters
+ * - Mass: kilograms to pounds and pounds to kilograms
+ * 
+ * If the input number is not provided, an alert is shown to prompt the user to enter a number.
+ * 
+ * @returns {void}
+ */
 function convert() {
     vol=""
     len=""
